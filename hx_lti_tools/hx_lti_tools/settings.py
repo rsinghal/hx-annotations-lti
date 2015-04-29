a61b9c1bb744ee7efc144c864333d7e4a36b0fd5
rsinghal <rashmi.s.singhal@gmail.com>
2015-04-29 15:47:02 -0400

    remove mysql db

--- hx_lti_tools/hx_lti_tools/settings.py
+++ hx_lti_tools/hx_lti_tools/settings.py
@@ -66,10 +66,8 @@ WSGI_APPLICATION = 'hx_lti_tools.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django_db',
-	'USER': 'root',
-	'PASSWORD': 'bu5egkeShy7Gphqf',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
